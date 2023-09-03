29ae5d865a4e2685e815e85c49792f963eca0833
Bikcraft <felipebaiano04@gmail.com>
2023-09-03 16:40:02 -0300

    Adicionando events js

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const ativaClass = document.getElementById("titulo");
-let inputs = document.getElementById("inputNunber");
+const inputs = document.getElementById("inputNunber");
 
-const ramdoNuber = Math.round(Math.random() * 10);
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+let randoNuber = Math.round(Math.random() * 10);
 let xAttempts = 1;
+
+//functin callback
 
 inputs.addEventListener("focus", focusHandler);
 inputs.addEventListener("focusout", focusHandler);
 
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
 function focusHandler() {
   ativaClass.classList.toggle("Ativaleds");
 }
+//Eventos
 
 function buttonHandler(event) {
   event.preventDefault();
 
-  if (Number(inputs.value) == ramdoNuber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
+  if (Number(inputs.value) == randoNuber) {
+    toggleScreen();
 
-    document.querySelector(".screen2 .numeros ").innerText = `${xAttempts}`;
+    screen2.querySelector(" .numeros ").innerText = `${xAttempts}`;
   }
+  inputs.value = "";
   xAttempts++;
 }
+
+function rendomResetClick() {
+  toggleScreen();
+  randoNuber = Math.round(Math.random() * 10);
+  xAttempts = 1;
+}
+
+btn1.addEventListener("click", buttonHandler);
+btn2.addEventListener("click", rendomResetClick);
+
+
+
+/*Desafios
+  Cria um limite no input entre 0 e 10 
+
+
+
+*/
